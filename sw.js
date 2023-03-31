3f6de4481891cd8c8432412c58194791511d949a
RonaldoGPP <104438683+RonaldoGPP@users.noreply.github.com>
2023-03-31 08:41:16 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,77 +1,58 @@
-const CACHE_NAME = 'mdl-portfolio-cache-v1';
-
-const urlsToCache = [
-  "./",
-    "./index.html",
-    "./blog.html",
-    "./about.html",
-    "./contact.html",
-    "./portfolio-example01.html",
-    "./styles.css",
-    "./app.js",
-    "./images/about-header.jpg",
-    "./images/contact-image.jpg",
-    "./images/example-blog01.jpg",
-    "./images/example-blog02.jpg",
-    "./images/example-blog03.jpg",
-    "./images/example-blog04.jpg",
-    "./images/example-blog05.jpg",
-    "./images/example-blog06.jpg",
-    "./images/example-blog07.jpg",
-    "./images/example-work01.jpg",
-    "./images/example-work02.jpg",
-    "./images/example-work03.jpg",
-    "./images/example-work04.jpg",
-    "./images/example-work05.jpg",
-    "./images/example-work06.jpg",
-    "./images/example-work07.jpg",
-    "./images/example-work08.jpg",
-    "./images/example-work09.jpg",
-    "./images/footer-background.png",
-    "./images/header-bg.jpg",
-    "./images/logo.png",
-    "./images/photo-wide.jpg",
-    "./images/photo.jpg",
-    "./images/portfolio-example-01.jpg",
-    "./images/portfolio-example-02.jpg",
-    "./images/portfolio-example-03.jpg",
-    "./images/portfolio-example-04.jpg",
-    "./images/portfolio-example-05.jpg",
-    "./images/portfolio-example-06.jpg"
+var CACHE_NAME = 'mdl-portfolio-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles/main.css',
+  '/scripts/main.js',
+  '/images/about-header.jpg',
+  '/images/contact-image.jpg',
+  '/images/example-blog01.jpg',
+  '/images/example-blog02.jpg',
+  '/images/example-blog03.jpg',
+  '/images/example-blog04.jpg',
+  '/images/example-blog05.jpg',
+  '/images/example-blog06.jpg',
+  '/images/example-blog07.jpg',
+  '/images/example-work01.jpg',
+  '/images/example-work02.jpg',
+  '/images/example-work03.jpg',
+  '/images/example-work04.jpg',
+  '/images/example-work05.jpg',
+  '/images/example-work06.jpg',
+  '/images/example-work07.jpg',
+  '/images/example-work08.jpg',
+  '/images/example-work09.jpg',
+  '/images/footer-background.png',
+  '/images/header-bg.jpg',
+  '/images/logo.png',
+  '/images/photo-wide.jpg',
+  '/images/photo.jpg',
+  '/images/portfolio-example-01.jpg',
+  '/images/portfolio-example-02.jpg',
+  '/images/portfolio-example-03.jpg',
+  '/images/portfolio-example-04.jpg',
+  '/images/portfolio-example-05.jpg',
+  '/images/portfolio-example-06.jpg'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
+      .then(function(response) {
         if (response) {
           return response;
         }
-
-        return fetch(event.request)
-          .then(response => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {return response;
-            }
-      
-            const responseToCache = response.clone();
-      
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-      
-            return response;
-            })
-            .catch(error => {
-              console.log('Error fetching data from network', error);
-            });
-        })
-      );
-      });
+        return fetch(event.request);
+      })
+  );
+});
